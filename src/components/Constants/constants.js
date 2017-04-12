48aab7455bba53a2ac36c55bf6bc5698ea3c6f92
sunby <359568366@qq.com>
2017-04-12 17:17:39 +0800

    提交在全部题打完之后显示 扣分必须上传图片

--- src/components/Constants/constants.js
+++ src/components/Constants/constants.js
@@ -1,6 +1,7 @@
 /**
  * Created by sunbingyi on 2017/3/14.
  */
-export const BaseUrl = 'http://115.28.28.219:8080/POSMPlatform/';
+//export const BaseUrl = 'http://115.28.28.219:8080/POSMPlatform/';
+export const BaseUrl = 'http://10.0.0.3:8080/POSMPlatform/';
 export const SizePerPage = 10;
 //export const BaseUrl = 'http://10.0.0.12:8080/POSMPlatform/';
