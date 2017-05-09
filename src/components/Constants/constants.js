1b214fc9afbdd9fdc27cee55ca8fe1782f3d5edb
sunby <359568366@qq.com>
2017-05-09 15:27:38 +0800

    change baseurl

--- src/components/Constants/constants.js
+++ src/components/Constants/constants.js
@@ -1,8 +1,8 @@
 /**
  * Created by sunbingyi on 2017/3/14.
  */
-//export const BaseUrl = 'http://115.159.78.97:8080/POSMPlatform/';
+export const BaseUrl = 'http://115.159.78.97:8080/POSMPlatform/';
 //export const BaseUrl = 'http://10.0.0.3:8080/POSMPlatform/';
-export const BaseUrl = 'http://121.43.34.185:7522/POSMPlatform/';
+//export const BaseUrl = 'http://121.43.34.185:7522/POSMPlatform/';
 export const SizePerPage = 10;
 //export const BaseUrl = 'http://10.0.0.12:8080/POSMPlatform/';
